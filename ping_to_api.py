5fb0a264043c4da4dc3456c1c207e9a6a8ffaa7a
Andrey <sebkd@mail.ru>
2022-05-26 22:14:58 +0300

    Final

--- ping_to_api.py
+++ ping_to_api.py
@@ -26,9 +26,6 @@ def get_full_path(urlbase, function):
     """Путь с учетом функции API"""
     return urlbase + function
 
-def get_responce(responce):
-    if isinstance()
-
 
 def login_to_api(urlbase, headers, login, enc_pass):
     """Вход в API"""
@@ -40,6 +37,7 @@ def login_to_api(urlbase, headers, login, enc_pass):
     url_post = get_query_params(urlbase=url, params=params)
     response = requests.post(url=url_post, headers=headers)
     return response
+
 
 def logout_from_api(urlbase, headers):
     url = get_full_path(urlbase=urlbase, function=SIGN_OUT)
@@ -65,26 +63,46 @@ def get_call_type_list_for_client(urlbase, headers):
     response = requests.get(url=url, headers=headers)
     return response
 
+
 def register_call(urlbase, headers, user_id, calltype_id, urgency_id, call_summary_name):
+    url = get_full_path(urlbase=urlbase, function=REGISTER_CALL)
     params = {
         'UserID': user_id,
-
+        'CallTypeID': calltype_id,
+        'UrgencyID': urgency_id,
+        'CallSummaryName': call_summary_name,
+        'HTMLDescription': 'Беда'
     }
+    url_post = get_query_params(urlbase=url, params=params)
+    response = requests.post(url=url_post, headers=headers)
+    return response
+
 
 def main(login, password, url, headers):
     encrypted_password = get_password_encrypted(password=password, key=13)
     data_input = login_to_api(urlbase=url, headers=headers, login=login, enc_pass=encrypted_password)
 
-    data_info_auth = json_get_info_auth(urlbase=url, headers=headers)
-    data_json_info_auth = data_info_auth.json()
     # забираем значение UserID
-    user_id = data_json_info_auth['UserID']
-    get_urgent_list(urlbase=url, headers=headers)
+    data_info_auth = get_info_auth(urlbase=url, headers=headers)
+    data_json_info_auth = data_info_auth.json()
+    user_id = data_json_info_auth.get('UserID')
+
+    # срочность заявки
+    data_urgent_list = get_urgent_list(urlbase=url, headers=headers)
+    data_json_urgent_list = data_urgent_list.json()
+    urgency_id = data_json_urgent_list[2].get('ID')
+
     data_call_type = get_call_type_list_for_client(urlbase=url, headers=headers)
     data_json_call_type = data_call_type.json()
-    # здесь забираем через ID и Name тип заявки и айди-зявки
+    # название заявки
+    call_summary_name = data_json_call_type[3].get('Name')
+    # ИД типа заявки
+    calltype_id = data_json_call_type[3].get('ID')
 
+    register_call(urlbase=url, headers=headers, user_id=user_id, calltype_id=calltype_id,
+                  urgency_id=urgency_id, call_summary_name=call_summary_name)
 
+    logout_from_api(urlbase=url, headers=headers)
 
 
 if __name__ == '__main__':
